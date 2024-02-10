115e92c8409a6bbfb7788dec1e0ecd95eadb7cca
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-02-09 16:42:53 -0800

    continued work

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,8 +1,8 @@
 const { MongoClient } = require("mongodb");
 const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
 });
 
 var _db;
--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ const dbo = require("./db/conn");
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
+    console.log(`Server is running on port: ${port}`);
+
     if (err) console.error(err);
   });
   console.log(`Server is running on port: ${port}`);
